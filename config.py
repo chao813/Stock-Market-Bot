ab59b54bf9a87bc71369b4dd0dc04fd5d9136343
chao813 <chaoo.yuan@gmail.com>
2020-07-01 00:18:05 -0400

    add logger to config

--- config.py
+++ config.py
@@ -1,4 +1,6 @@
 import os
+import logging
+
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -6,5 +8,5 @@ load_dotenv()
 DEBUG = True
 PATH = os.path.dirname(os.path.realpath(__file__))
 DATABASE = PATH + "/stocks.db"
-
-
+LOG_FILE_PATH = "/logs/stock_market_bot.log"
+LOGGER_LEVEL = logging.DEBUG
