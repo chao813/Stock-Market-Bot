b17b0b3b5732b8dc870fd113779bb30b5b15f983
chao813 <chaoo.yuan@gmail.com>
2020-06-30 23:47:32 -0400

    added flask error handling

--- app.py
+++ app.py
@@ -1,10 +1,18 @@
-from flask import Flask
+from flask import Flask, jsonify
 from api.api import api_bp
 
 
 app = Flask(__name__)
 app.config.from_object("config")
 app.register_blueprint(api_bp, url_prefix="/api")
+
+@app.errorhandler(404)
+def not_found(error):
+    return jsonify({'status': 'Not Found',}), 404
+
+@app.errorhandler(500)
+def internal_error(error):
+    return jsonify({'status': 'Internal Server Error',}), 500
 
 @app.route("/")
 def index():
